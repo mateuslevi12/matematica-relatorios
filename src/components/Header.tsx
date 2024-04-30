cbc5b5732dfb9a3936454d7a6ee90f033d503fc4
mateuslevi12 <mateuslevialencar32@gmail.com>
2024-04-30 12:00:04 -0300

    jungle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import jungle from '../pages/Group 26.png'
+
 export function Header() {
     return (
         <>
-            <div className="bg-green-400 h-20"></div>
+            <div className="bg-green-400 h-20 flex items-center p-4">
+                <img className='h-14' src={jungle} alt="" />
+            </div>
         </>
     )
 }
