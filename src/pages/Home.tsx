119b98aeb181bc6c9956f30ad453e75e3aff08b3
mateuslevi12 <mateuslevialencar32@gmail.com>
2024-05-01 17:31:34 -0300

    asds

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,16 +25,16 @@ export function Home() {
             <div className="flex flex-col gap-2">
                 <h1 className="text-xl">Relatorios</h1>
                 <div className={`gap-2 ${styles.relatorios}`}>
-                    <CardComponent titulo='Relatorio 1 - Educação' descricao='Cidadãos que frequentam a escola menos os que tiveram dengue.' icon={<IoBookOutline />} quantidade={238} />
-                    <CardComponent titulo='Relatorio 2 - Saúde' descricao='Cidadãos que frequentaram o posto de saúde menos os que não utilizam onibus.' icon={<GiHealthNormal />} quantidade={191} />
-                    <CardComponent titulo='Relatorio 3 - Mobilidade' descricao='Cidadãos que utilizaram o transporte público e não tiveram dengue.' icon={<IoBus />} quantidade={183} />
+                    <CardComponent titulo='Relatorio 1 - Educação' descricao='Cidadãos que frequentam a escola menos os que tiveram dengue.' icon={<IoBookOutline />} quantidade={236} />
+                    <CardComponent titulo='Relatorio 2 - Saúde' descricao='Cidadãos que frequentaram o posto de saúde menos os que não utilizam onibus.' icon={<GiHealthNormal />} quantidade={189} />
+                    <CardComponent titulo='Relatorio 3 - Mobilidade' descricao='Cidadãos que utilizaram o transporte público e não tiveram dengue.' icon={<IoBus />} quantidade={182} />
                     <CardComponent titulo='Relatorio 4 - Educação e Saúde' descricao='Cidadãos que frequentaram a escola e tiveram dengue.' icon={<IoBookOutline />} quantidade={25} />
                     <CardComponent titulo='Relatorio 5 - Educação e Mobilidade' descricao='Cidadãos que frequentaram a escola e utilizaram transporte público.' icon={<GiHealthNormal />} quantidade={23} />
                     <CardComponent titulo='Relatorio 6 - Saúde e Mobilidade' descricao='Cidadãos que frequentaram o posto de saúde e utilizaram transporte público.' icon={<IoBus />} quantidade={11} />
                     <CardComponent titulo='Relatorio 7 - Saúde, Mobilidade, Educação' descricao='Cidadãos que frequentaram o posto de saúde, utilizaram transporte público e frequentaram a escola.' icon={<IoBookOutline />} quantidade={2} />
-                    <CardComponent titulo='Relatorio 8' descricao='Cidadãos que frequentaram o posto de saúde, mas não utilizaram transporte público.' icon={<GiHealthNormal />} quantidade={191} />
-                    <CardComponent titulo='Relatorio 9' descricao='Cidadãos que frequentaram o posto de saúde, mas não frequentaram a escola.' icon={<IoBus />} quantidade={177} />
-                    <CardComponent titulo='Relatorio 10' descricao='Cidadãos que frequentaram o posto de saúde, mas não frequentaram a escola, nem utilizaram transporte público.' icon={<IoBus />} quantidade={168} />
+                    <CardComponent titulo='Relatorio 8' descricao='Cidadãos que frequentaram o posto de saúde, mas não utilizaram transporte público.' icon={<GiHealthNormal />} quantidade={189} />
+                    <CardComponent titulo='Relatorio 9' descricao='Cidadãos que frequentaram o posto de saúde, mas não frequentaram a escola.' icon={<IoBus />} quantidade={175} />
+                    <CardComponent titulo='Relatorio 10' descricao='Cidadãos que frequentaram o posto de saúde, mas não frequentaram a escola, nem utilizaram transporte público.' icon={<IoBus />} quantidade={164} />
                 </div>
             </div>
         </div>
